58bc080e418d9970cdabd58249a052d398d711fa
Anas <437100219@Student.ksu.edu.sa>
2019-10-04 05:20:55 +0300

    imges

--- front-end-2/src/pages/login.js
+++ front-end-2/src/pages/login.js
@@ -3,6 +3,9 @@ import '../Login.css';
 import loading from '../loading.svg'
 import { withRouter } from 'react-router-dom'
 import axios from 'axios';
+import ksuLogo from '../ksu.jpg'
+import ExampleComponent from "react-rounded-image";
+import Background from '../ksu-background.jpg';
 
 class Login extends Component {
   state = {
@@ -11,70 +14,70 @@ class Login extends Component {
     isLoading: false,
     isErorr: false,
   }
-
   componentDidUpdate() {
     console.log(this.state);
   }
 
   handleLogin = () => {
-    this.setState({isLoading: true, isErorr: false});
-    axios.post('http://localhost:5012/getStudentInformation',{id:this.state.id,password:this.state.password}).then(res => {
+    this.setState({ isLoading: true, isErorr: false });
+    axios.post('http://localhost:5012/getStudentInformation', { id: this.state.id, password: this.state.password }).then(res => {
       console.log(res.data.user.name)
       localStorage.setItem('auth', JSON.stringify(res.data));
       this.props.history.push('/');
     }).catch((error) => {
       console.log('there is an error');
-      this.setState({isLoading: false,isErorr: true})
+      this.setState({ isLoading: false, isErorr: true })
     })
-    
+
   }
 
   render() {
     return (
       <div>
-        <div className="card-container text-center align-center d-flex justify-content-center">
-          <div className="card login-card" style={(this.state.isErorr) ? {maxHeight: '420px'} : {maxHeight: '350px'}}>
-            <form className="form-signin" dir="rtl" onSubmit={(e) => {e.preventDefault()}}>
-              <img className="logo" src="/images/favicons/ms-icon-150x150.png" alt="" width={72} height={72} />
+                  
+          <div className="card-container text-center align-center d-flex justify-content-center">
+          <div className="card login-card" style={(this.state.isErorr) ? { maxHeight: '420px' } : { maxHeight: '350px' }}>
+            <form className="form-signin" dir="rtl" onSubmit={(e) => { e.preventDefault() }}>
+            <img src={ksuLogo} width={72} height={72} rounded/>
+              <img className="logo" src="/images/favicons/ms-icon-150x150.png" alt="" width={72} height={72}  />
               <h1 className="login-title h3 mb-3 font-weight-normal force-font">تسجيل الدخول</h1>
               {(this.state.isErorr) ? <div class="alert alert-warning" role="alert">
-  الرقم الجامعي أو كلمة السر غير صحيحة 
-</div> : <div></div>}
+                الرقم الجامعي أو كلمة السر غير صحيحة
+                 </div> : <div></div>}
               <label htmlFor="student_id" className="sr-only">الرقم الجامعي</label>
-              <input 
-                name="student_id" 
-                type="text" 
-                id="student_id" 
-                className="form-control" 
-                placeholder="الرقم الجامعي" 
-                value={this.state.id} 
-                
-                required 
-                autofocus 
+              <input
+                name="student_id"
+                type="text"
+                id="student_id"
+                className="form-control"
+                placeholder="الرقم الجامعي"
+                value={this.state.id}
+
+                required
+                autofocus
                 onChange={(e) => {
-                  this.setState({id: e.target.value})
-                }}/>
+                  this.setState({ id: e.target.value })
+                }} />
               <label htmlFor="inputPassword" className="sr-only">كلمة السر</label>
-              <input 
-                name="password" 
-                type="password" 
+              <input
+                name="password"
+                type="password"
                 id="inputPassword"
-                className="form-control" 
-                placeholder="كلمة السر" 
+                className="form-control"
+                placeholder="كلمة السر"
                 required
                 onChange={(e) => {
-                  this.setState({password: e.target.value})
-                }}/>
-              <button 
-                style={{padding: '10px 0px', minHeight: '61px'}}
-                className="btn login-button btn-lg btn-success btn-block" 
+                  this.setState({ password: e.target.value })
+                }} />
+              <button
+                style={{ padding: '10px 0px', minHeight: '61px' }}
+                className="btn login-button btn-lg btn-success btn-block"
                 type="submit"
                 onClick={this.handleLogin}
                 disabled={this.state.isLoading}
-                >
-               
-                  {(this.state.isLoading) ? <img alt="Loading" height={40} src={loading} /> : 'تسجيل'}
-                  
+              >
+
+                {(this.state.isLoading) ? <img alt="Loading" height={40} src={loading} /> : 'تسجيل'}
               </button>
             </form>
           </div>
